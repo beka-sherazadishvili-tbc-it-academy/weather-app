a6bbbdc93bb582964cec8699439001bb1a25b812
milena-mamadiani-tbc-it-academy <devwme0@gmail.com>
2025-11-09 16:01:24 +0400

    feat(not-found): add not-found section

---
+++ features/scripts/not-found.js
@@ -0,0 +1,14 @@
+const notFound = document.querySelector('.not__found');
+const sections = document.querySelectorAll("main section");
+
+function invisible(inputValue){
+    if(!inputValue){
+        sections.forEach(sections => {
+            if(sections.classList.contains('not__found')) {
+                notFound.style.display = 'flex';
+            } else {
+                sections.style.display = 'none';
+            }
+        })
+    }
+}
