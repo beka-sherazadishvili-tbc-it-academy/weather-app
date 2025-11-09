69d5fbe05d054fd3547a303cd9d2a6d413a6e787
burdu <23202223@ibsu.edu.ge>
2025-11-09 17:25:40 +0400

    feat(header): add some features to header

--- features/scripts/weather-hero.js
+++ features/scripts/weather-hero.js
@@ -1,11 +1,13 @@
 const temperatureCounts = document.querySelectorAll(".temperature__count");
+
 function initializeTemperature() {
     if (currentWeatherData) {
+        console.log(`${currentWeatherData.currentTemp} ______________WEATHER HERO`)
     for (let i = 0; i < temperatureCounts.length; i++) {
       if (i === 0) {
-        temperatureCounts[i].textContent = `${currentWeatherData.hourlyTemp[i]}°C`;
+        temperatureCounts[i].innerText = `${currentWeatherData.currentTemp}°C`;
       } else {
-        temperatureCounts[i].textContent = `${currentWeatherData.hourlyTemp[i - 1]}°C`;
+        temperatureCounts[i].innerText = `${currentWeatherData.hourlyTemp[i - 1]}°C`;
       }
     }
   }
